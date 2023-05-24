41348c81e31b8d60625128b372b4af5fa3daa34a
Francis Lagares <francis.lagares@gmail.com>
2023-05-24 19:45:39 +0200

    test: add cypress types config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import './App.css';
+
 import { useState } from 'react';
 
 import reactLogo from './assets/react.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,26 @@
 import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-import { expect, afterEach } from 'vitest';
+import { afterEach, expect, vi } from 'vitest';
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
+
+// mocking methods which are not implemented in JSDOM
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // Deprecated
+      removeListener: vi.fn(), // Deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
+});
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
