cdeb3897f1caea98afc529f0e0c9e847d6a33270
Francis Lagares <francis.lagares@gmail.com>
2023-09-05 15:02:00 +0200

    fix: stylelint config

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 
-import '@/css/index.css';
+import '@/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
