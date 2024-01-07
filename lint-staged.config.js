fbbb36912b8beaf035b13db841d9298b9dd68d2d
Francis Lagares <francis.lagares@gmail.com>
2024-01-07 20:44:12 +0100

    chore: update dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'yarn type-check',
+  '**/*.ts?(x)': () => 'pnpm type-check',
   // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': filenames => `yarn lint ${filenames.join(' ')}`,
+  '**/*.(ts|js)?(x)': filenames => `pnpm lint ${filenames.join(' ')}`,
   // Run Stylelint on changes to CSS/SCSS/LESS files
 };
